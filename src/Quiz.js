5087afbded4727a8a652663e7645807e5bee15ed
Brian Harrison <brharr@microsoft.com>
2018-07-02 13:43:36 -0400

    Added Click Handlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,9 +17,15 @@ class Quiz extends Component {
         
         return (
             <div>
-                {isQuizEnd ? (<QuizEnd />) : (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>)}
+                {isQuizEnd ? (<QuizEnd />) : (<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>)}
             </div>
         );
+    }
+
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1}
+        });
     }
 }
 
